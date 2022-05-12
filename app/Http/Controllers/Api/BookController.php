3041cab5404876e26fd3bb5cff592cd5d3f199c1
Phan Nguyễn Dũng Mạnh (BU37.Vinh) <manh.phan@bnksolution.com>
2022-05-12 14:07:19 +0700

    curd books

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use App\Http\Resources\Book as BookResource;
+use Database\Seeders\BookSeeder;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $book = Book::all();
+        return BookResource::collection($book);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $book = Book::firstOrCreate($request->all());
+        return new BookResource($book);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $book = Book::find($id);
+        return new BookResource($book);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $book = Book::find($id);
+        $book->update($request->all());
+        return new BookResource($book);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        try {
+            Book::whereId($id)->delete();
+            return response()->json(["destroy" => "OK"], \Illuminate\Http\Response::HTTP_OK);
+        } catch (\Illuminate\Database\QueryException $e) {
+            return response()->json(["destroy" => "ERROR"], \Illuminate\Http\Response::HTTP_OK);
+        }
+    }
+}
