39310712320ca2674ab1cf6d90146c1dcc3b791b
Phan Nguyễn Dũng Mạnh (BU37.Vinh) <manh.phan@bnksolution.com>
2022-05-13 20:37:56 +0700

    fix name model book

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class Book extends JsonResource
+class BookResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
